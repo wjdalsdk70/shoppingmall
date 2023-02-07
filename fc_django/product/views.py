6375232033b09403b3cb84f804bdcbd4e5ad44e2
Jeongmin Lee <kyungheeyj45@naver.com>
2023-02-07 20:16:07 +0900

    상품 주문하기

--- fc_django/product/views.py
+++ fc_django/product/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import FormView
 from .models import Product
+from .forms import RegisterForm
+from order.forms import RegisterForm as OrderForm
 # Create your views here.
 
 
@@ -8,3 +11,21 @@ class ProductList(ListView):
     model = Product
     template_name = 'product.html'
     context_object_name = 'product_list'
+
+
+class ProductCreate(FormView):
+    template_name = 'register_product.html'
+    form_class = RegisterForm
+    success_url = '/product/'
+
+
+class ProductDetail(DetailView):
+    template_name = 'product_detail.html'
+    queryset = Product.objects.all()  # 필터도 가능
+    context_object_name = 'product'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['form'] = OrderForm(self.request)
+        return context
