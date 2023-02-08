1b996934db69c24c083f2c73cf8f3a394dc6b3a3
Jeongmin Lee <kyungheeyj45@naver.com>
2023-02-08 20:16:37 +0900

    Decorator 사용(권한 설정)

--- fc_django/fc_django/urls.py
+++ fc_django/fc_django/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from fcuser.views import index, RegisterView, LoginView
+from fcuser.views import index, logout, RegisterView, LoginView
 from product.views import ProductList, ProductCreate, ProductDetail
 from order.views import OrderCreate, OrderList
 
@@ -24,6 +24,7 @@ urlpatterns = [
     path('', index),
     path('register/', RegisterView.as_view()),
     path('login/', LoginView.as_view()),
+    path('logout/', logout),
     path('product/', ProductList.as_view()),
     path('product/create/', ProductCreate.as_view()),
     path('product/<int:pk>/', ProductDetail.as_view()),
--- fc_django/product/views.py
+++ fc_django/product/views.py
@@ -4,6 +4,8 @@ from django.views.generic.edit import FormView
 from .models import Product
 from .forms import RegisterForm
 from order.forms import RegisterForm as OrderForm
+from django.utils.decorators import method_decorator
+from fcuser.decorators import admin_required
 # Create your views here.
 
 
@@ -13,6 +15,7 @@ class ProductList(ListView):
     context_object_name = 'product_list'
 
 
+@method_decorator(admin_required, name='dispatch')
 class ProductCreate(FormView):
     template_name = 'register_product.html'
     form_class = RegisterForm
