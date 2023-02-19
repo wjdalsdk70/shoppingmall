8510ddfaeaadb4ee9ca8918919682284d8d4f1bc
Jeongmin Lee <kyungheeyj45@naver.com>
2023-02-19 20:00:24 +0900

    추가 리스트 페이지

--- fc_django/order/admin.py
+++ fc_django/order/admin.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
 from .models import Order
+from django.utils.html import format_html
 # Register your models here.
 
 
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('fcuser', 'product')
+    list_filter = ('status',)
+    list_display = ('fcuser', 'product', 'styled_status')
+
+    def styled_status(self, obj):
+        if obj.status == '환불':
+            return format_html(f'<span style="color:red">{obj.status}</span>')
+        if obj.status == '결제완료':
+            return format_html(f'<span style="color:green">{obj.status}</span>')
+        return format_html(f'<b>{obj.status}</b>')
+
+    styled_status.short_description = '상태'
 
 
 admin.site.register(Order, OrderAdmin)
