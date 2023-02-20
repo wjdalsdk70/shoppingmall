a38aec21913cce08c6f60cca269823efd50cd16e
Jeongmin Lee <kyungheeyj45@naver.com>
2023-02-20 19:04:57 +0900

    커스텀필드 포맷팅

--- fc_django/product/admin.py
+++ fc_django/product/admin.py
@@ -1,10 +1,26 @@
 from django.contrib import admin
 from .models import Product
+from django.utils.html import format_html
+from django.contrib.humanize.templatetags.humanize import intcomma
 # Register your models here.
 
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price')
+    list_display = ('name', 'price_format', 'styled_stock')
+
+    def price_format(self, obj):
+        price = intcomma(obj.price)
+        return f'{price} 원'
+
+    def styled_stock(self, obj):
+        stock = obj.stock
+        if stock <= 50:
+            stock = intcomma(stock)
+            return format_html(f'<b><span style="color:red">{stock} 개</span></b>')
+        return format_html(f'<b>{intcomma(stock)} 개</b>')
+
+    price_format.short_description = '가격'
+    styled_stock.short_description = '재고'
 
 
 admin.site.register(Product, ProductAdmin)
